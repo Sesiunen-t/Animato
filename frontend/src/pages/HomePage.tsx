57f67c70095c7d0885e5136711d8d0b77e3f2108
Alex <alexmihoc@yahoo.com>
2023-06-17 17:00:49 +0300

    feature: added SVGs and background

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import VideoPlayer from '../components/VideoPlayer';
 import VideoGenerator from '../components/VideoGenerator';
+import flowerUp from '../assets/flowerUp.svg';
+import flowerDown from '../assets/flowerDown.svg';
+import bigWeb from '../assets/bigWeb.svg';
+import smallWeb from  '../assets/smallWeb.svg';
+import animato from '../assets/animato.svg'
 
 const HomePage: React.FC = () => {
-  const videoSrcURL = "https://www.example.com/video.mp4"; // this should be fetched from your API
 
   return (
     <div className="home-page">
-      <h1 className="text-3xl font-bold mb-4">Zhvr yn gvtnav!</h1>
-      <VideoPlayer videoSrcURL={videoSrcURL} />
+        <img src={flowerUp} alt="flowerUp" className='z-10 absolute top-0 left-0 h-[55%] w-[55%] -mx-[19%]'/>
+        <img src={flowerDown} alt="flowerDown" className= 'z-10 absolute bottom-0 right-0 h-[60%] w-[55%] -mx-[20%]'/>
+        <img src={bigWeb} alt="spiderWeb" className="absolute bottom-0 right-0"/>
+        <img src={smallWeb} alt="spiderWeb" className="absolute bottom-0 right-0 h-[70%] w-[70%]"/>
+        <div className="w-full h-full flex justify-center items-center">
+            <img src={animato} alt="animato" className="w-[80%] h-[80%]"/>
+        </div>
       <VideoGenerator />
     </div>
   );
