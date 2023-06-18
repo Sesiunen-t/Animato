d953f3e8b26ece5f1fce52ead08d63c493885467
arking115 <rusalexandru115@gmail.com>
2023-06-18 11:47:58 +0300

    90% done design

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -9,9 +9,9 @@ import animato from '../assets/animato.svg';
 const HomePage: React.FC = () => {
 
   return (
-    <div className="home-page">
+    <div className="home-page w-full">
         <img src={flowerUp} alt="flowerUp" className='z-10 absolute top-0 left-0 h-[55%] w-[55%] -mx-[19%]'/>
-        <img src={flowerDown} alt="flowerDown" className= 'z-10 absolute bottom-0 right-0 h-[60%] w-[55%] -mx-[20%]'/>
+        <img src={flowerDown} alt="flowerDown" className= 'z-10 absolute h-[60%] w-[55%] -mx-[20%] bottom-0 right-[20%]'/>
         <img src={bigWeb} alt="spiderWeb" className="absolute bottom-0 right-0"/>
         <img src={smallWeb} alt="spiderWeb" className="absolute bottom-0 right-0 h-[70%] w-[70%]"/>
         <div className="w-full h-full flex justify-center items-center bg-red-500">
